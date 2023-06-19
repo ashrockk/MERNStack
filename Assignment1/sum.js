e2c959b49eb0b329d6129f90627b9d29e1052888
SaurabhKarki <saurabh.karki98176@gmail.com>
2023-06-19 19:03:28 +0545

    Added two numbers

---
+++ Assignment1/sum.js
@@ -0,0 +1,23 @@
+// Task 1
+const  a = Number(prompt("Enter a value of the first integer: "));
+const  b = Number(prompt("Enter the value of second integer: "));
+function sum(a,b)
+{
+    if(a == b)
+    {
+        return 3*(a+b);
+    }
+    else
+    return a+b;
+}
+const c = sum(a,b);
+console.log(c);
+
+
+// const  a = Number (prompt("Enter a value of the first integer: "));
+// const  b = Number(prompt("Enter the value of second integer: "));
+// if(a==b)
+// c = 3*(a+b)
+// else
+// c = a+b;
+// console.log(c);
