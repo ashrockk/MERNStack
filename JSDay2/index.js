4ab8363acd33abdde747e9f1f2cbb5a608f9efbb
SaurabhKarki <saurabh.karki98176@gmail.com>
2023-06-20 13:23:05 +0545

    We learnt about arrays

--- JSDay2/index.js
+++ JSDay2/index.js
@@ -11,3 +11,25 @@ const person ={
 const c = person.firstName;
 const a = person.fullName();
 console.log(c,a);
+
+//Arrays
+
+const arr =["Saab", "Volvo" , "BMW", {name:"alto"}];
+
+// access the first item
+const b = arr[0];
+console.log(b);
+
+// accessing the last item instead.
+const e = arr[arr.length-1]; 
+console.log(e);
+
+
+// add an item to the end of an array
+
+console.log(arr.length);
+
+
+//What's the difference between array and objects fundamentally?
+// answer: Index ley define garcha. 
+
