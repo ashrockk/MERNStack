017485744859da51253cd023ca15bb624b4f5f29
SaurabhKarki <saurabh.karki98176@gmail.com>
2023-06-21 14:37:38 +0545

    Random commit.

--- node-example/app.js
+++ node-example/app.js
@@ -1,10 +1,13 @@
 // app.js
 const http = require('http');
 const currentDateTime = require("./myFirstModule");
+const uc = require("upper-case");
+
 
 http.createServer((request, response) => {
 //   response.writeHead(200, { 'Content-Type': 'text/html' });
-  response.end('Hello World!' + currentDateTime.myDateTime());
+const message = uc.upperCase("hello world");
+  response.end(message + currentDateTime.myDateTime());
 }).listen(8080);
 
 console.log("Application running on port 8080");
