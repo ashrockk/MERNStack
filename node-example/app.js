e120b626ad25aa424a99825c8c006230bb03babf
SaurabhKarki <saurabh.karki98176@gmail.com>
2023-06-21 10:48:31 +0545

    Ran first app in Node.js

---
+++ node-example/app.js
@@ -0,0 +1,9 @@
+// app.js
+const http = require('http');
+
+http.createServer((req, res) => {
+  res.writeHead(200, { 'Content-Type': 'text/html' });
+  res.end('Hello World!');
+}).listen(8080);
+
+console.log('App running on port 8080');
