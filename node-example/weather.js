165a7c2f4f07e9fb409fd9576d868a36556f96c7
SaurabhKarki <saurabh.karki98176@gmail.com>
2023-06-22 14:48:53 +0545

    making a weather app using Weather API

---
+++ node-example/weather.js
@@ -0,0 +1,14 @@
+const axios = require("axios");
+
+// make axios call here
+
+const fetchWeatherData = async() = {};
+
+// usage example (inside an async Function):
+
+async function getWeather({
+
+})
+
+//Call the async function to fetch the display the weather data
+getWeather();
