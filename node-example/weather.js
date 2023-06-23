0f38baaca8343948fb73ede61edc5dbae616a0d8
SaurabhKarki <saurabh.karki98176@gmail.com>
2023-06-23 17:48:02 +0545

    made(copied) a weather app

--- node-example/weather.js
+++ node-example/weather.js
@@ -1,14 +1,36 @@
 const axios = require("axios");
+const moment = require("moment");
 
 // make axios call here
+const fetchWeatherData = async () => {
+  try {
+    const response = await axios(
+      "https://api.open-meteo.com/v1/forecast?latitude=27.70&longitude=85.32&hourly=temperature_2m&daily=sunrise,sunset&forecast_days=1&timezone=auto"
+    );
 
-const fetchWeatherData = async() = {};
+    // My customization
+    const customizedWeatherReport = {
+      date: moment().format("LLLL"),
+      temperature: String(
+        JSON.stringify(
+          response.data.hourly.temperature_2m[new Date().getHours()]
+        ).concat(" C")
+      ),
+      sunrise: moment(response.data.daily.sunrise.toString()).format("LLL"),
+      sunset: moment(response.data.daily.sunset.toString()).format("LLL"),
+    };
+    // My Customization
+    return { weather: customizedWeatherReport };
+  } catch (error) {
+    throw new Error("Failed to fetch weather data.");
+  }
+};
 
-// usage example (inside an async Function):
+// Usage example (inside an async function):
+async function getWeather() {
+  const data = await fetchWeatherData();
+  console.log(data);
+}
 
-async function getWeather({
-
-})
-
-//Call the async function to fetch the display the weather data
+// Call the async function to fetch and display the weather data
 getWeather();
